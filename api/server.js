fd1256bedf9c2e46519f40c44f8ad00eb35b2d49
Chenxi <lin.chenxi14@gmail.com>
2023-05-25 21:29:15 -0400

    added get request for each route

--- api/server.js
+++ api/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config()
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const app = express();
 
 
@@ -19,33 +18,96 @@ mongoose
 
 const userSchema = mongoose.Schema({
     Title: String,
-    Description: String
+    Description: String,
+    Date: Date
+});
+
+const workSchema = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Date: Date
+});
+
+const vacationSchema = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Date: Date
 })
 
 const User = mongoose.model('user', userSchema);
+const Work = mongoose.model('work', workSchema);
+const Vacation = mongoose.model('vacation', vacationSchema);
 
-app.post('/', (req, res) => {
-    const makeList = new User({
+app.post('/home', (req, res) => {
+    const userList = new User({
         Title: req.body.title,
-        Description: req.body.description
+        Description: req.body.description,
+        Date: new Date()
     })
-    makeList.save().then(() => {
+    userList.save().then(() => {
         console.log('added')
-        res.redirect('http://localhost:3000')
+        res.redirect('http://localhost:3000/home')
     });
 })
 
-app.get('/update', async (req, res) => {
+app.post('/work', (req, res) => {
+    const workList = new Work({
+        Title: req.body.title,
+        Description: req.body.description,
+        Date: new Date()
+    });
+    workList.save().then((result) => {
+        console.log(result)
+        res.redirect('http://localhost:3000/work')
+    })
+});
+
+app.post('/vacation', (req, res) => {
+    const vacationList = new Vacation({
+        Title: req.body.title,
+        Description: req.body.description,
+        Date: new Date()
+    });
+    vacationList.save().then((result) => {
+        console.log(result)
+        res.redirect('http://localhost:3000/vacation')
+    })
+})
+
+
+
+app.get('/home', async (req, res) => {
     try {
         const allUser = await User.find({})
+        res.status(200).send(allUser)
+        console.log('showList')
+    } catch (err) {
+        console.log(err)
+        res.status(500).send()
+    }
+});
+
+app.get('/work', async (req, res) => {
+    try {
+        const allUser = await Work.find({})
         res.status(200).send(allUser)
         console.log(allUser)
     } catch (err) {
         console.log(err)
         res.status(500).send()
     }
-    
-})
+});
+
+app.get('/vacation', async (req, res) => {
+    try {
+        const allUser = await Vacation.find({})
+        res.status(200).send(allUser)
+        console.log(allUser)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send()
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
